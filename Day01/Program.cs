8aa8b07fede9a84b9325539e7e9f5dd284231931
Jenn Hernes <jenn_hernes@hotmail.com>
2022-11-02 22:08:44 +0200

    add solution for day 1

---
+++ Day01/Program.cs
@@ -0,0 +1,16 @@
+﻿var input = File.ReadAllLines("./input.txt").First();
+
+var sum = 0;
+var length = input.Length;
+var halflen = length / 2;
+
+for (int i = 0; i < length; i++)
+{
+    //if (input[i] == input[(i + 1) % length])
+    if (input[i] == input[(i + halflen) % length])
+    {
+        sum += input[i] - '0';
+    }
+}
+
+Console.WriteLine(sum);
