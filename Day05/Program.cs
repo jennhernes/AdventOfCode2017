ac4f2eb5638ba32a330967eee84c3765aa89bc50
Jenn Hernes <jenn_hernes@hotmail.com>
2022-11-04 18:03:10 +0200

    add solution to day 5

---
+++ Day05/Program.cs
@@ -0,0 +1,22 @@
+﻿var input = File.ReadAllLines("./input.txt");
+
+var jumps = input.Select(x => int.Parse(x)).ToList();
+
+var i = 0;
+long steps = 0;
+while (i > -1 && i < jumps.Count)
+{
+    var offset = jumps[i];
+    if (offset >= 3)
+    {
+        jumps[i]--;
+    }
+    else
+    {
+        jumps[i]++;
+    }
+    i += offset;
+    steps++;
+}
+
+Console.WriteLine(steps);
