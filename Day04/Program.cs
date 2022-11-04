2e73e3ce98f09bc3d535bd38e24ff03260cf2d92
Jenn Hernes <jenn_hernes@hotmail.com>
2022-11-04 17:51:18 +0200

    add solution for day 4

---
+++ Day04/Program.cs
@@ -0,0 +1,26 @@
+﻿var input = File.ReadAllLines("./input.txt");
+int valid = 0;
+foreach (var line in input)
+{
+    var words = line.Split(' ');
+    var dict = new Dictionary<string, int>();
+    foreach (var word in words)
+    {
+        var arr = word.ToArray();
+        Array.Sort(arr);
+        var sorted = new string(arr);
+        // if (dict.ContainsKey(word))
+        if (dict.ContainsKey(sorted))
+        {
+            valid++;
+            break;
+        }
+        else
+        {
+            // dict.Add(word, 1);
+            dict.Add(sorted, 1);
+        }
+    }
+}
+
+Console.WriteLine(input.Length-valid);
