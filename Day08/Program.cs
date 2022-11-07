dd0401e76f1bde43a28d8d9b419c8a09b51a8fc9
Jenn Hernes <jenn_hernes@hotmail.com>
2022-11-07 10:11:35 +0200

    add solution for day 8

---
+++ Day08/Program.cs
@@ -0,0 +1,43 @@
+﻿var input = File.ReadAllLines("./input.txt");
+
+var registers = new Dictionary<string, int>();
+var currentMax = 0;
+foreach (var instruction in input)
+{
+    var tokens = instruction.Split(' ');
+    if (!registers.ContainsKey(tokens[0]))
+    {
+        registers.Add(tokens[0], 0);
+    }
+
+    if (tokens.Length > 3 && !registers.ContainsKey(tokens[4]))
+    {
+        registers.Add(tokens[4], 0);
+    }
+
+    if (tokens.Length > 4)
+    {
+        var limit = int.Parse(tokens[6]);
+        if ((registers[tokens[4]] == limit 
+            && (tokens[5] == "==" || tokens[5] == ">=" || tokens[5] == "<="))
+            || (registers[tokens[4]] > limit
+            && (tokens[5] == ">" || tokens[5] == ">="))
+            || (registers[tokens[4]] < limit
+            && (tokens[5] == "<" || tokens[5] == "<="))
+            || (registers[tokens[4]] != limit
+            && (tokens[5] == "!=")))
+        {
+            if (tokens[1] == "inc")
+            {
+                registers[tokens[0]] += int.Parse(tokens[2]);
+            }
+            else 
+            {
+                registers[tokens[0]] -= int.Parse(tokens[2]);
+            }
+        }
+    }
+    currentMax = Math.Max(currentMax, registers.Values.Max());
+}
+
+Console.WriteLine(currentMax);
